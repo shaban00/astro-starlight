574ef98e2f7f95752f93e1da9e91db06e72a4518
Shaban Mohammedsaani Hassan <shaban00hassan@gmail.com>
2025-09-25 20:43:50 -0400

    Added custom domain

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,8 +4,7 @@ import starlight from '@astrojs/starlight';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://shaban00.github.io',
-	base: '/astro-starlight',
+	site: 'https://astro.shaban.cc',
 	integrations: [
 		starlight({
 			title: 'My Docs',
